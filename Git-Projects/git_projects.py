509fb2f289801bce29fd2f4af7ac1d9657f0e87f
Vítor Araújo <vitorjna@gmail.com>
2026-02-11 16:46:00 +0000

    Add files via upload

--- Git-Projects/git_projects.py
+++ Git-Projects/git_projects.py
@@ -11,12 +11,15 @@ class ExtendedEnum(Enum):
         return list(map(lambda c: c.value, cls))
 
 class Commands(IntEnum):
-    PULL = 1
-    PUSH = 2
-    STATUS = 3
-    CHECKOUT = 4
-    LOG = 5
-    POP = 6
+    CHECKOUT = 1
+    COMMIT = 2
+    LOG = 3
+    POP = 4
+    PULL = 5
+    PUSH = 6
+    STATUS = 7
+    CMD = 8
+    OPEN = 9
 
 class Tools(ExtendedEnum):
     TORT = "tortoise"
@@ -24,29 +27,47 @@ class Tools(ExtendedEnum):
 
 def getCommandForTool(command, tool):
     match command:
-        case Commands.PULL: return Commands.PULL.name.lower()
-        case Commands.PUSH: return Commands.PUSH.name.lower()
-        case Commands.STATUS:
-            match tool:
-                case Tools.TORT: return "repostatus"
-                case _: return Commands.STATUS.name.lower()
         case Commands.CHECKOUT:
             match tool:
                 case Tools.TORT: return "switch"
+                case Tools.GIT:
+                    branch = input("Enter branch name: ")
+                    return f"{Commands.CHECKOUT.name.lower()} {branch}"
                 case _: return Commands.CHECKOUT.name.lower()
+
+        case Commands.COMMIT:
+            match tool:
+                case Tools.GIT:
+                    message = input("Enter commit message: ")
+                    return f"commit -a -m \"{message}\""
+                case _: return Commands.COMMIT.name.lower()
+
+        case Commands.LOG: return Commands.LOG.name.lower()
+
         case Commands.POP:
             match tool:
                 case Tools.GIT:  return "stash pop"
                 case Tools.TORT: return "stashpop"
                 case _: return Commands.CHECKOUT.name.lower()
-        case Commands.LOG: return Commands.LOG.name.lower()
+
+        case Commands.PULL: return Commands.PULL.name.lower()
+
+        case Commands.PUSH: return Commands.PUSH.name.lower()
+
+        case Commands.STATUS:
+            match tool:
+                case Tools.TORT: return "repostatus"
+                case _: return Commands.STATUS.name.lower()
+
+        case Commands.CMD: return input("Enter command: ")
+
         case _: return ""
 
 
 if __name__ == "__main__":
 
     while True:
-        project_name = input(f"Select a Project: {', '.join(PROJECTS.keys())}: ").upper()
+        project_name = input(f"\nSelect a Project: {', '.join(PROJECTS.keys())}: ").upper()
 
         if project_name == 'Q':
             break
@@ -61,7 +82,11 @@ if __name__ == "__main__":
         tool_to_use = Tools(GIT_TOOL)
 
         while True:
-            command_name = input(f"Select a Command: {Commands._member_names_}, BACK: ").upper()
+            git_commands = sorted([name for name in Commands._member_names_ if Commands[name] < Commands.CMD])
+            other_commands = sorted([name for name in Commands._member_names_ if Commands[name] >= Commands.CMD])
+            commands_display = f'[{", ".join(git_commands)}], [{", ".join(other_commands)}], [BACK], [Q]'
+
+            command_name = input(f"\n[{project_name}] Select a Command: {commands_display}: ").upper()
             if command_name == 'Q':
                 os.system('pause')
                 exit()
@@ -69,21 +94,31 @@ if __name__ == "__main__":
             if command_name == 'BACK':
                 break
 
+            if command_name == 'OPEN':
+                first_repo = next(iter(repos))
+                full_repo_path = Path(reposLocation) / first_repo
+                print(f"Opening {full_repo_path} in Explorer...")
+                os.startfile(full_repo_path)
+                continue
+
             if command_name not in Commands._member_names_:
                 print("Invalid command selected.")
                 continue
 
-            command_action = getCommandForTool(Commands[command_name], tool_to_use)
+            command_enum = Commands[command_name]
+            command_action = getCommandForTool(command_enum, tool_to_use)
             print("Selected command: " + command_action)
 
             for repo in repos:
                 full_repo_path = Path(reposLocation) / repo
-                if tool_to_use == Tools.TORT:
+                if command_enum == Commands.CMD:
+                    cmd = f'start /wait cmd /c "cd /d "{full_repo_path}" && {command_action} && pause"'
+                elif tool_to_use == Tools.TORT:
                     cmd = f'"{PATH_TORTOISE}" /path:"{full_repo_path}" /command:{command_action}'
                 else:
-                    cmd = f'git -C "{full_repo_path}" {command_action}'
+                    cmd = f'start /wait cmd /c "git -C "{full_repo_path}" {command_action} && pause"'
 
                 print(f'Running: {cmd}')
-                subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL)
+                subprocess.run(cmd, shell=True)
 
     os.system('pause')
